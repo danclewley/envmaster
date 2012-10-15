783a92370c08c5ec1a095adce1a8850558014e5b
Sam Gillingham <gillingham.sam@gmail.com>
2012-10-15 19:50:43 +1300

    Fixed license and license notices

--- envmaster/src/envmaster/envmasterformat.py
+++ envmaster/src/envmaster/envmasterformat.py
@@ -4,6 +4,22 @@ In an effort to maintain the look and feel of the orginal
 this module takes information about the size of the terminal
 and fills it up with text turned into columns or tables.
 """
+# This file is part of EnvMaster
+# Copyright (C) 2012  Sam Gillingham
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import math
 import fcntl
 import termios
